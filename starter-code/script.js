be65d7fbd5236eb98ed537a58b2a9543c74f967f
Yonas Tesfu <tesfu.yonas1350@gmail.com>
2022-08-21 23:10:47 +0300

    script file added for the first time

--- starter-code/script.js
+++ starter-code/script.js
@@ -1,1 +1,25 @@
 "use strict";
+const home_btn = document.querySelector(".destination");
+home_btn.addEventListener("click", function () {
+  console.log("Hello yonas");
+});
+const moon_btn = document.querySelector(".moon");
+const mars_btn = document.querySelector(".mars");
+const europa_btn = document.querySelector(".europa");
+const titan_btn = document.querySelector(".titan");
+moon_btn.addEventListener("click", changeTabs);
+mars_btn.addEventListener("click", changeTabs);
+europa_btn.addEventListener("click", changeTabs);
+titan_btn.addEventListener("click", changeTabs);
+
+function changeTabs(btn) {
+  if (!btn.path[0].classList.contains("active")) {
+    moon_btn.classList.remove("active");
+    mars_btn.classList.remove("active");
+    europa_btn.classList.remove("active");
+    titan_btn.classList.remove("active");
+    btn.path[0].classList.add("active");
+  }
+
+  //
+}
